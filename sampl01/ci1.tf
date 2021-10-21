9f1b9b7826470fc2084a2ea3f2a7c306e0ae9ee2
Nawaz <rnbabar@me.com>
2021-10-20 21:04:24 -0700

    terraform2

--- sampl01/ci1.tf
+++ sampl01/ci1.tf
@@ -1,9 +1,9 @@
 provider "aws" {
     region = "us-east-1"
-  
+ 
 }
 resource "aws_instance" "carts" {
-    ami = "ami-052ed334670027b3"
+    ami = "ami-0e4e4b2f188e91845"
     instance_type = "t2.micro"
     
 }
