a684133276146573dfb610d494ab868b67dd37e3
Nawaz <rnbabar@me.com>
2021-10-21 16:44:14 -0700

    terraform2

--- sampl02/ci_sc.tf
+++ sampl02/ci_sc.tf
@@ -22,7 +22,7 @@ resource "aws_security_group" "allow_tls" {
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ]
+      ipv6_cidr_blocks = []
       self             = false
       prefix_list_ids  = []
       security_groups  = []
