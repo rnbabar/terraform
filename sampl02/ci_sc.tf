9993fc41554e66a515e89a41f3768abea87130d4
Nawaz <rnbabar@me.com>
2021-10-21 16:43:06 -0700

    terraform2

--- sampl02/ci_sc.tf
+++ sampl02/ci_sc.tf
@@ -22,8 +22,8 @@ resource "aws_security_group" "allow_tls" {
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      self             = true
+      ipv6_cidr_blocks = ]
+      self             = false
       prefix_list_ids  = []
       security_groups  = []
 
@@ -37,8 +37,8 @@ resource "aws_security_group" "allow_tls" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      self             = true
+      ipv6_cidr_blocks = []
+      self             = false
       prefix_list_ids  = []
       security_groups  = []
     }
