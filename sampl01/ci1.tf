bed86abff2e10b0f77b9259bd6188a74d4bd3506
Nawaz <rnbabar@me.com>
2021-10-20 20:54:07 -0700

    terraform2

--- sampl01/ci1.tf
+++ sampl01/ci1.tf
@@ -2,12 +2,12 @@ provider "aws" {
     region = "us-east-1"
   
 }
-resource "aws_instance" "cart" {
+resource "aws_instance" "carts" {
     ami = "ami-074df373d6bafa625"
     instance_type = "t2.micro"
     
 }
 
 output "public_ip" {
-    value = aws_instance.cart.public_ip
+    value = aws_instance.carts.public_ip
 }
