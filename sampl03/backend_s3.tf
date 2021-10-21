9c1f47ff10ba79acae190659500b77e61f4a25a5
Nawaz <rnbabar@me.com>
2021-10-21 16:59:53 -0700

    terraform2

---
+++ sampl03/backend_s3.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+    region = "us-east-1"
+ 
+}
+resource "aws_instance" "carts" {
+    ami = "ami-0e4e4b2f188e91845"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+    
+}
+
+terraform {
+  backend "s3" {
+    bucket = "rnbabar-storage"
+    key    = "sampl03/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
