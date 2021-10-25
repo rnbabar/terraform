9138492133b8ef4ceee31246052c6aae16d57824
Nawaz <rnbabar@me.com>
2021-10-25 01:05:20 -0700

    terraform2

---
+++ mod_loops/loop.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+ 
+}
+resource "aws_instance" "carts" {
+    count = 2
+    ami = "ami-0e4e4b2f188e91845"
+    instance_type = "t2.micro"
+    tag = {
+        Name = "Catalogue"
+    }
+    
+}
+
+output "public_ip" {
+    value = aws_instance.carts.public_ip
+}
