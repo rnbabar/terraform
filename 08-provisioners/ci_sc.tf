ce531735eb7d570bb24c7a3fc8cfb871c0f47de5
Nawaz <rnbabar@me.com>
2021-10-29 18:02:28 -0700

    terraform2

--- 08-provisioners/ci_sc.tf
+++ 08-provisioners/ci_sc.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "carts" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-    provisioner "remote-exec" {
+    provisioner  "remote-exec" {
       connection {
         user = "centos"
         password= "Devops321"
